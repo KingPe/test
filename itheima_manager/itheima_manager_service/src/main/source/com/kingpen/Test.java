a14178e234c2b6600f04c3f8e8ca0b55fb1c9b73
KingPe <kingpen01@163.com>
2018-04-20 09:26:15 +0800

    张三提交了第二次

---
+++ itheima_manager/itheima_manager_service/src/main/source/com/kingpen/Test.java
@@ -0,0 +1,13 @@
+package com.kingpen;
+
+/**
+ * @author kingpen
+ * @version 1.0
+ * @description com.kingpen
+ * @date 2018/4/20
+ */
+public class Test {
+    public void outPut(){
+        System.out.println("张三输出语句");
+    }
+}
